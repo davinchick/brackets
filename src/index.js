c74fe8908c3175e1553708770e173e4b09c52d54
davinchick <57950327+davinchick@users.noreply.github.com>
2020-03-05 12:37:23 +0300

    Add: test solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    function flatten(arr) {
+        return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), []);
+    }
+    let bracketsArr = flatten(bracketsConfig);
+    let newArray = ( str + bracketsArr.toString()).replace(/[^\(\)\{\}\[\]\|]/g, '').split('');
+
+    if (newArray.length===0){ return false;}
+
+    function areTheyOrdered(string){
+        var i, 
+            l = string.length, 
+            count = 0, 
+            el, 
+            latest, 
+            bracketsStack = [];
+
+        for(i=0; i<l; i++){
+            el = string[i];
+            if  (el==="{" || el==="(" || el==="[" || (el==="|" && latest!=='|')){
+                    bracketsStack.push(el);
+                    latest = el;
+                }
+            else if(el === '}' || el === ")" || el === "]" || el==="|" ){
+                if(latest){
+                    if( (el === '}' && latest === "{") || (el === ']' && latest === "[") || (el === ')' && latest === "(") || (el === '|' && latest === "|") ){
+                        bracketsStack.pop();
+                        latest = bracketsStack.length > 0 ? bracketsStack[bracketsStack.length - 1] : undefined;
+                    }
+                }else{
+                    return false;
+                }
+            }
+        }
+        return bracketsStack.length===0 && count%2===0;
+    }
+    return areTheyOrdered(newArray);
 }
